06fd5cdbc1b8eecf5529fb17cf4f8a895d836a59
Leonel Francisco <leonelftacisco@hotmail.com>
2026-03-16 22:50:50 +0000

    update: arquitecture pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+
+export const Home = () => {
+
+    return (
+        <Text>
+            <Text>Home</Text>
+        </Text>
+    );
+}
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+ 
+
+
+ export const Settings = () => {
+
+    return (
+        <View>
+            <Text>Settings Page</Text>
+        </View>
+    );
+ }
